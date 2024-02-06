7fa9391378c955cda2fff5eb85c470effc88b05a
redder97 <red9gb@gmail.com>
2024-02-07 02:45:29 +0800

    Added environments

--- src/app/pocket-base.service.ts
+++ src/app/pocket-base.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core'
 import PocketBase from 'pocketbase'
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PocketBaseService {
   constructor() {
-    this.pb = new PocketBase('http://127.0.0.1:8090')
+    this.pb = new PocketBase(environment.pocketbase.url)
   }
   pb
   async viewTest() {
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  pocketbase: {
+    url: 'http://127.0.0.1:8090',
+  },
+}
