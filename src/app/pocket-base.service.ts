27825bc4a5f895c3671ee2fc8c8745f6f9b3cfd2
akosiJ <akosij@J>
2024-02-06 21:28:34 +0800

    npm installed pocketbase

---
+++ src/app/pocket-base.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import PocketBase from 'pocketbase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PocketBaseService {
+  constructor(
+  ){
+    this.pb = new PocketBase('http://127.0.0.1:8090'); 
+  }
+  pb
+  async viewTest (){
+    return await this.pb.collection('users').getOne('RECORD_ID', {
+      expand: 'relField1,relField2.subRelField',
+    });
+  }
+
+}
